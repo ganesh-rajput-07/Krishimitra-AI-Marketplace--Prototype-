098ad473ffadeaf08f44f84d2da8904a7d12fa51
Ganesh Rajput <ganeshrajput7045@gmail.com>
2025-02-25 23:17:06 +0530

    Update settings.py

--- krishimitra/settings.py
+++ krishimitra/settings.py
@@ -27,7 +27,7 @@ import dj_database_url  # Ensure this is also imported
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [ * ]
+ALLOWED_HOSTS = [ "*"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
